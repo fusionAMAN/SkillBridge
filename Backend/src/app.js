57d6fe8d4ea16ac404837b09455b5d4996dadf40
fusionAMAN <786rockaman@gmail.com>
2025-05-02 23:17:31 +0530

    update

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://delicate-mochi-3c8476.netlify.app",
+    origin: "https://symphonious-banoffee-acfddb.netlify.app",
     credentials: true,
   })
 );
